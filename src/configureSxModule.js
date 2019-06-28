68b26dd5d00f44cbff3950badb13d0c953d20ec7
eva <eva.de.haas@liones.nl>
2019-06-28 10:59:02 +0200

    Upgrade to SDK 7.7

--- src/configureSxModule.js
+++ src/configureSxModule.js
@@ -1,42 +1,37 @@
-define([
-	'fontoxml-families/configureAsFrame',
-	'fontoxml-families/configureAsSheetFrame',
-	'fontoxml-families/configureAsTitleFrame',
-	'fontoxml-families/createMarkupLabelWidget',
-	'fontoxml-families/createRelatedNodesQueryWidget',
-	'fontoxml-localization/t'
-], function (
-	configureAsFrame,
-	configureAsSheetFrame,
-	configureAsTitleFrame,
-	createMarkupLabelWidget,
-	createRelatedNodesQueryWidget,
-	t
-) {
-	'use strict';
+import configureAsFrame from 'fontoxml-families/src/configureAsFrame.js';
+import configureAsSheetFrame from 'fontoxml-families/src/configureAsSheetFrame.js';
+import configureAsTitleFrame from 'fontoxml-families/src/configureAsTitleFrame.js';
+import createMarkupLabelWidget from 'fontoxml-families/src/createMarkupLabelWidget.js';
+import createRelatedNodesQueryWidget from 'fontoxml-families/src/createRelatedNodesQueryWidget.js';
+import t from 'fontoxml-localization/src/t.js';
 
-	return function configureSxModule (sxModule) {
-		// glossgroup
-		//     The <glossgroup> element may be used to contain multiple <glossentry> topics within a single
-		//     collection.
-		configureAsSheetFrame(sxModule, 'self::glossgroup', t('glossary'), {
-			titleQuery: './title//text()[not(ancestor::*[name() = ("sort-at", "draft-comment", "foreign", "unknown", "required-cleanup", "image")])]/string() => string-join()',
-			blockFooter: [
-				createRelatedNodesQueryWidget('descendant::fn[not(@conref) and fonto:in-inline-layout(.)]')
-			],
-			blockHeaderLeft: [
-				createMarkupLabelWidget()
-			]
-		});
+export default function configureSxModule(sxModule) {
+	// glossgroup
+	//     The <glossgroup> element may be used to contain multiple <glossentry> topics within a single
+	//     collection.
+	configureAsSheetFrame(sxModule, 'self::glossgroup', t('glossary'), {
+		titleQuery:
+			'./title//text()[not(ancestor::*[name() = ("sort-at", "draft-comment", "foreign", "unknown", "required-cleanup", "image")])]/string() => string-join()',
+		blockFooter: [
+			createRelatedNodesQueryWidget(
+				'descendant::fn[not(@conref) and fonto:in-inline-layout(.)]'
+			)
+		],
+		blockHeaderLeft: [createMarkupLabelWidget()]
+	});
 
-		// glossgroup nested in topic
-		configureAsFrame(sxModule, 'self::glossgroup[parent::*[fonto:dita-class(., "topic/topic")]]', undefined, {
+	// glossgroup nested in topic
+	configureAsFrame(
+		sxModule,
+		'self::glossgroup[parent::*[fonto:dita-class(., "topic/topic")]]',
+		undefined,
+		{
 			blockFooter: []
-		});
+		}
+	);
 
-		// title in glossary
-		configureAsTitleFrame(sxModule, 'self::title[parent::glossgroup]', undefined, {
-			fontVariation: 'document-title'
-		});
-	};
-});
+	// title in glossary
+	configureAsTitleFrame(sxModule, 'self::title[parent::glossgroup]', undefined, {
+		fontVariation: 'document-title'
+	});
+}
